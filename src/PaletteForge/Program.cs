3a7b9320b7094d7119fe06c62a859b3ff862abbe
Jayden Cowley <JACowley@afstores.com>
2026-01-11 13:34:12 -0700

    Created Navigation, ininitial prototyping of home page UI

--- src/PaletteForge/Program.cs
+++ src/PaletteForge/Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PaletteForge;
+using MudBlazor.Services;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddMudServices();
 
-await builder.Build().RunAsync();
+        await builder.Build().RunAsync();
+    }
+}
